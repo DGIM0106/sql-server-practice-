279d77c6773a3c1b69a299b6b6c253e6a03dd963
Daniel Icaza <danielgicaza@outlook.com>
2026-07-09 10:39:08 -0500

    Order by

--- Basic/firstquery.sql
+++ Basic/firstquery.sql
@@ -1,7 +1,27 @@
+--General query
 SELECT *
 FROM [dbo].[Empleados]
-WHERE [Departamento] = ''
-
+GO
+--Specific query
 SELECT [Nombre], [Sueldo]
 FROM [dbo].[Empleados]
-WHERE [Departamento] = 'Sistemas'
+WHERE [Departamento] = 'Sistemas';
+GO
+--More Specific query
+SELECT [Nombre], [Departamento], [Sueldo]
+FROM [dbo].[Empleados]
+WHERE ([Departamento] = 'Sistemas' AND [Sueldo] > 1600) OR ([Departamento] = 'Marketing');
+GO
+
+--Order By
+SELECT [Nombre], [Departamento], [Sueldo]
+FROM [dbo].[Empleados]
+WHERE ([Departamento] = 'Sistemas' AND [Sueldo] > 1600) OR ([Departamento] = 'Marketing')
+ORDER BY [Sueldo];
+GO
+--ORDER BY DESCENDENTE
+SELECT [Nombre], [Departamento], [Sueldo]
+FROM [dbo].[Empleados]
+WHERE ([Departamento] = 'Sistemas' AND [Sueldo] > 1600) OR ([Departamento] = 'Marketing')
+ORDER BY [Sueldo] DESC;
+GO
